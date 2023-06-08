e07d17b6413a5cc47a99c870734a27b348a3a757
ethanmitchell2057@gmail.com <ethanmitchell2057@gmail.com>
2023-06-08 19:32:53 +0100

    hh

--- cdn/script2.js
+++ cdn/script2.js
@@ -1,76 +1,74 @@
-  <script nonce="XpbG3KBh">
-                        requireLazy(["__bigPipe"], function (bigPipe) {
-                            bigPipe.onPageletArrive({
-                                displayResources: ["lUg7w0b"],
-                                content: { "m:chrome:schedulable-graph-search": { container_id: "u_0_j_6v" } },
-                                id: "m:chrome:schedulable-graph-search",
-                                phase: 7,
-                                last_in_phase: true,
-                                last_pagelet: true,
-                                all_phases: [63, 7],
-                                jsmods: {
-                                    elements: [["__elem_ad2bcfb1_0_1_hK", "u_0_i_Yf", 1]],
-                                    require: [
-                                        [
-                                            "MSearchTypeaheadNeueFlyout",
-                                            "init",
-                                            ["MSearchSimplificationNullstateResult", "MSearchSimplificationSeeMoreRecentResultsButton", "MSearchSimplificationTypedResult", "MSearchSimplificationHeader", "__elem_ad2bcfb1_0_1_hK"],
-                                            [
-                                                {
-                                                    typeaheadConfig: {
-                                                        sources: {
-                                                            bootstrapped: { src: "/typeahead/search/facebar/bootstrap/?filters%5B0%5D=keyword", source_key: "facebar3", max_results: 6 },
-                                                            nullstate: { src: "/typeahead/search/facebar/nullstate/", max_results: 20 },
-                                                            online: { src: "/typeahead/search/facebar/query/?context=facebar&grammar_version=bee09f93fa732cfa59a1cb6d9f450d3892424e49&viewer=0&rsp=search", max_results: 20, enable_caching: true },
-                                                        },
-                                                        typeaheadScubaInfo: null,
-                                                        disableLongerQueryCacheHit: true,
-                                                        compositeSourceConfig: {
-                                                            maxResults: 50,
-                                                            minCostForUserToSurfaceAtTop: 7,
-                                                            maxPositionOfEntityToDivideKeywords: 5,
-                                                            maxKeywordAboveEntities: 2,
-                                                            filterOutTypes: [],
-                                                            keepBootstrapped: true,
-                                                            enableLegacyStyleBucketization: true,
-                                                        },
-                                                        hasRecentSearches: false,
-                                                        numResultsToFetch: 13,
-                                                        initialNumRecentResultsToShow: 13,
-                                                        defaultNumTopResultsToShow: 3,
-                                                        keywordsOnlyEnabled: true,
-                                                        keywordBucketSize: 6,
+                   requireLazy(["__bigPipe"], function (bigPipe) {
+                                        bigPipe.onPageletArrive({
+                                            displayResources: ["lUg7w0b"],
+                                            content: { "m:chrome:schedulable-graph-search": { container_id: "u_0_j_6v" } },
+                                            id: "m:chrome:schedulable-graph-search",
+                                            phase: 7,
+                                            last_in_phase: true,
+                                            last_pagelet: true,
+                                            all_phases: [63, 7],
+                                            jsmods: {
+                                                elements: [["__elem_ad2bcfb1_0_1_hK", "u_0_i_Yf", 1]],
+                                                require: [
+                                                    [
+                                                        "MSearchTypeaheadNeueFlyout",
+                                                        "init",
+                                                        ["MSearchSimplificationNullstateResult", "MSearchSimplificationSeeMoreRecentResultsButton", "MSearchSimplificationTypedResult", "MSearchSimplificationHeader", "__elem_ad2bcfb1_0_1_hK"],
+                                                        [
+                                                            {
+                                                                typeaheadConfig: {
+                                                                    sources: {
+                                                                        bootstrapped: { src: "/typeahead/search/facebar/bootstrap/?filters%5B0%5D=keyword", source_key: "facebar3", max_results: 6 },
+                                                                        nullstate: { src: "/typeahead/search/facebar/nullstate/", max_results: 20 },
+                                                                        online: { src: "/typeahead/search/facebar/query/?context=facebar&grammar_version=bee09f93fa732cfa59a1cb6d9f450d3892424e49&viewer=0&rsp=search", max_results: 20, enable_caching: true },
+                                                                    },
+                                                                    typeaheadScubaInfo: null,
+                                                                    disableLongerQueryCacheHit: true,
+                                                                    compositeSourceConfig: {
+                                                                        maxResults: 50,
+                                                                        minCostForUserToSurfaceAtTop: 7,
+                                                                        maxPositionOfEntityToDivideKeywords: 5,
+                                                                        maxKeywordAboveEntities: 2,
+                                                                        filterOutTypes: [],
+                                                                        keepBootstrapped: true,
+                                                                        enableLegacyStyleBucketization: true,
+                                                                    },
+                                                                    hasRecentSearches: false,
+                                                                    numResultsToFetch: 13,
+                                                                    initialNumRecentResultsToShow: 13,
+                                                                    defaultNumTopResultsToShow: 3,
+                                                                    keywordsOnlyEnabled: true,
+                                                                    keywordBucketSize: 6,
+                                                                },
+                                                                currentProfileID: null,
+                                                                initialQuery: "",
+                                                                activityLogURI: "",
+                                                                groupName: "",
+                                                                behaviors: [],
+                                                                renderers: {
+                                                                    NullstateResult: { __m: "MSearchSimplificationNullstateResult" },
+                                                                    SeeMoreRecentResultsButton: { __m: "MSearchSimplificationSeeMoreRecentResultsButton" },
+                                                                    TypedResult: { __m: "MSearchSimplificationTypedResult" },
+                                                                    Header: { __m: "MSearchSimplificationHeader" },
+                                                                },
+                                                                root: { __m: "__elem_ad2bcfb1_0_1_hK" },
+                                                            },
+                                                        ],
+                                                    ],
+                                                    ["RequireDeferredReference", "unblock", [], [["FbtLogging", "IntlQtEventFalcoEvent", "MPageControllerImpl", "MPageFetcherImpl"], "sd"]],
+                                                    ["RequireDeferredReference", "unblock", [], [["FbtLogging", "IntlQtEventFalcoEvent", "MPageControllerImpl", "MPageFetcherImpl"], "css"]],
+                                                ],
+                                            },
+                                            hsrp: {
+                                                hsdp: { qexData: { "180": { r: null } } },
+                                                hblp: {
+                                                    consistency: { rev: 1007643455 },
+                                                    rsrcMap: {
+                                                        LWMJrwU: { type: "js", src: "https://z-m-static.xx.fbcdn.net/rsrc.php/v3/yY/r/UXTaMaWEJmB.js?_nc_x=Ij3Wp8lg5Kz", nc: 1 },
+                                                        UIx9zkZ: { type: "js", src: "https://z-m-static.xx.fbcdn.net/rsrc.php/v3iRWc4/y2/l/en_GB/snXJ4hLcz3G.js?_nc_x=Ij3Wp8lg5Kz", nc: 1 },
                                                     },
-                                                    currentProfileID: null,
-                                                    initialQuery: "",
-                                                    activityLogURI: "",
-                                                    groupName: "",
-                                                    behaviors: [],
-                                                    renderers: {
-                                                        NullstateResult: { __m: "MSearchSimplificationNullstateResult" },
-                                                        SeeMoreRecentResultsButton: { __m: "MSearchSimplificationSeeMoreRecentResultsButton" },
-                                                        TypedResult: { __m: "MSearchSimplificationTypedResult" },
-                                                        Header: { __m: "MSearchSimplificationHeader" },
-                                                    },
-                                                    root: { __m: "__elem_ad2bcfb1_0_1_hK" },
                                                 },
-                                            ],
-                                        ],
-                                        ["RequireDeferredReference", "unblock", [], [["FbtLogging", "IntlQtEventFalcoEvent", "MPageControllerImpl", "MPageFetcherImpl"], "sd"]],
-                                        ["RequireDeferredReference", "unblock", [], [["FbtLogging", "IntlQtEventFalcoEvent", "MPageControllerImpl", "MPageFetcherImpl"], "css"]],
-                                    ],
-                                },
-                                hsrp: {
-                                    hsdp: { qexData: { "180": { r: null } } },
-                                    hblp: {
-                                        consistency: { rev: 1007643455 },
-                                        rsrcMap: {
-                                            LWMJrwU: { type: "js", src: "https://z-m-static.xx.fbcdn.net/rsrc.php/v3/yY/r/UXTaMaWEJmB.js?_nc_x=Ij3Wp8lg5Kz", nc: 1 },
-                                            UIx9zkZ: { type: "js", src: "https://z-m-static.xx.fbcdn.net/rsrc.php/v3iRWc4/y2/l/en_GB/snXJ4hLcz3G.js?_nc_x=Ij3Wp8lg5Kz", nc: 1 },
-                                        },
-                                    },
-                                },
-                                allResources: ["lUg7w0b", "hZeK99e", "6qUA1gl", "LWMJrwU", "UIx9zkZ", "PDQbV7k", "Gd5GBLJ"],
-                            });
-                        });
-                    </script>
+                                            },
+                                            allResources: ["lUg7w0b", "hZeK99e", "6qUA1gl", "LWMJrwU", "UIx9zkZ", "PDQbV7k", "Gd5GBLJ"],
+                                        });
+                                    });
